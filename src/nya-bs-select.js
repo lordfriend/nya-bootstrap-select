5990374aacef46671161372e3489302d21f28161
Ricardo Casares <Ricardo.X.Casares.-ND@disney.com>
2015-02-22 18:11:18 -0300

    if an item is selected previously, focus will be set on that item when the dropdown is opened. fixes #40

--- src/nya-bs-select.js
+++ src/nya-bs-select.js
@@ -581,6 +581,13 @@ nyaBsSelect.directive('nyaBsSelect', ['$parse', '$document', '$timeout', 'nyaBsC
             firstLiElement = dropdownMenu.children().eq(dropdownMenu.children().length - 1);
           }
 
+          // focus on selected element
+          for(var i = 0; i < dropdownMenu.children().length; i++) {
+            if(dropdownMenu.children().eq(i).hasClass('selected')) {
+              return dropdownMenu.children().eq(i)[0];
+            }
+          }
+
           if(firstLiElement.hasClass('nya-bs-option') && !firstLiElement.hasClass('disabled') && !firstLiElement.hasClass('not-match')) {
             return firstLiElement[0];
           } else {
