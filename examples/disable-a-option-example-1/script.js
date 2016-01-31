ca16404458141fcfefa71acebf3e76f6053323b4
Sakakino Konomi <konomi@Sakakinos-Mac-Pro.local>
2016-01-31 11:15:08 +0800

    Updates

--- examples/disable-a-option-example-1/script.js
+++
@@ -1,8 +0,0 @@
-(function(angular) {
- 'use strict';
-  var myApp = angular.module('DisableAOptionExample1App', ['nya.bootstrap.select']);
-
-  myApp.controller('MainController', ['$scope', function($scope){
-  $scope.options=['Alpha', 'Bravo', 'Charlie'];
-  }]);
-})(window.angular);
