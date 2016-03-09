e43efcf44b5228c5c0ae0010b54011e236182fd6
Josh McKearin <jpmckearin@live.com>
2016-03-09 14:34:15 -0600

    Revert "fix functionality with updating parent scope items"
    This reverts commit c1891d2fcc1b71dda4d992e711e7ab097bc136d4.

--- src/nya-bs-select.js
+++ src/nya-bs-select.js
@@ -743,7 +743,7 @@ nyaBsSelect.directive('nyaBsSelect', ['$parse', '$document', '$timeout', '$compi
 
             // update view value regardless
             ngCtrl.$setViewValue(viewValue);
-            $scope.$apply();
+            $scope.$digest();
 
             updateButtonContent();
           }
@@ -791,7 +791,7 @@ nyaBsSelect.directive('nyaBsSelect', ['$parse', '$document', '$timeout', '$compi
           }
           // update view value regardless
           ngCtrl.$setViewValue(viewValue);
-          $scope.$apply();
+          $scope.$digest();
 
           if(!isMultiple) {
             // in single selection mode. close the dropdown menu
