eaf017fc3bf05db50292af9ef1579c79daccf594
lordfriend <lordfriend@outlook.com>
2016-06-14 14:10:16 +0800

    Updates

---
+++ examples/selected-text-format-example-1/script.js
@@ -0,0 +1,17 @@
+(function(angular) {
+ 'use strict';
+  var myApp = angular.module('SelectedTextFormatExample1App', ['nya.bootstrap.select']);
+
+  myApp.controller('MainController', ['$scope', function($scope){
+  $scope.options1 = [
+  'Alpha',
+  'Bravo',
+  'Charlie',
+  'Golf',
+  'Hotel',
+  'Juliet',
+  'Kilo',
+  'Lima'
+];
+  }]);
+})(window.angular);
